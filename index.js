91b196f0a9e1b7d7c7c0c39295926dd352dc9f10
SoorajModi <soomodi@gmail.com>
2020-09-09 15:39:12 -0400

    Add javascript file

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('the-button').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate');
+});
+
+document.getElementById('the-button2').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate');
+});
+
+document.getElementById('the-button3').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate');
+});
