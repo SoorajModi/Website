538cd8bc75cb398fd47be6cfbfe93959ee644da0
SoorajModi <soomodi@gmail.com>
2020-09-09 16:38:53 -0400

    Add folder for js files

---
+++ js/about.js
@@ -0,0 +1,19 @@
+document.getElementById('about_drop').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate90');
+});
+
+document.getElementById('about_drop2').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate90');
+});
+
+document.getElementById('about_drop3').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate90');
+});
+
+document.getElementById('about_drop4').addEventListener('click', function() {
+  var icon = this.querySelector('.right i.fa');
+  icon.classList.toggle('rotate90');
+});
