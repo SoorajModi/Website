fbe16d905b1be422bc2432c9927966bc8833be5a
Sooraj <soomodi@gmail.com>
2020-09-20 08:08:48 +0100

    Add 404 page and UI improvements

--- server.js
+++ server.js
@@ -12,9 +12,34 @@ app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.post("/experience", function(req, res){
-  res.sendFile(path.join(__dirname, "/static/html/work_experience.html"));
+app.get("/experience", function(req, res){
+  res.sendFile(path.join(__dirname, "/static/experience.html"));
 });
+
+app.get("/education", function(req, res){
+  res.sendFile(path.join(__dirname, "/static/education.html"));
+});
+
+app.get("/volunteering", function(req, res){
+  res.sendFile(path.join(__dirname, "/static/volunteering.html"))
+});
+
+app.get("/resume", function(req, res) {
+  res.sendFile(path.join(__dirname, "/static/resume.html"))
+});
+
+app.get("/contact", function(req, res) {
+  res.sendFile(path.join(__dirname, "/static/contact.html"))
+});
+
+// app.get("/about", function(req, res) {
+//   res.sendFile(path.join(__dirname, "/static/about.html"))
+// });
+
+// 404 Errors
+app.use(function (req, res, next) {
+  res.status(404).sendFile(path.join(__dirname, "/static/404.html"))
+})
 
 app.listen(8008, function(){
   console.log("Server started on port 8008");
