6f59b7b77738bbfbfec8d5394e53e4697dfa741f
SoorajModi <soomodi@gmail.com>
2021-01-25 00:16:17 -0500

    Website updates

--- app.js
+++ app.js
@@ -28,46 +28,6 @@ const blogScheme = {
 
 const BlogPost = mongoose.model("BlogPost", blogScheme);
 
-// const proj = new BlogPost({
-//   title: "sooraj modi website",
-//   description: "soorajmodi.com was the first website I have developed and made publicly available. It was a great learning " +
-//    "experience and I learned a lot about web development through the process such as HTML, CSS, JavaScript, MongoDB, routing," +
-//    "web design, and more. This blog posts goes over some of the skills I've learned through the process, and an inside peak" +
-//    "into some of the challenges and features of this website.",
-//   body: ["soorajmodi.com is my very first website and I created this to showcase my skills, experience, and other " +
-//          "accomplishments I am proud of. It was a learning experience on full-stack development and how to create a web application " +
-//          "that anyone in the world can interact with. There were some challenges, which forced me to fortify my understanding of web development " +
-//          "concepts. Some of the technologies I worked with were full stack development, HTML, CSS, JavaScript, Node, ESLint, Prettier, Embedded JavaScript Templating, " +
-//          "CI/CD, GitHub actions, Heroku, CloudFlare, DNS, and more.",
-//          "The basic building blocks of web development are HTML, CSS, and JavaScript. You could build virtually any frontend project " +
-//          "with these three. So that's where I began my web development journey. I learned HTML and CSS through various online tutorials " +
-//          "as well as just messing around with the inspect element option in the browser. This was fairly straight forward to learn, " +
-//          "but there are still features and little intricacies I encounter every now and again. JS was a bit more complicated." +
-//          "I was able to pick it up fairly quickly following my first year of University. At that point in time, I had a foundational " +
-//          "understanding of programming, and it was substantially easier to understand than C, which is what they taught in my Computer " +
-//          "Science classes.",
-//          "Next, I had to learn about Node.js and backend development. My knowledge from my first internship at Bell Canada, a telecommunications company, " +
-//          "really helped here. I learned a ton about backend development, including routing, writing modular, clean, code. Further, I was able to fill in " +
-//          "the gaps of my knowledge with Udemy courses, StackOverflow, and various googling.",
-//          "A major component of this website is the incorporation of my personal blog. In order to accomplish this, I had to learn about a database to manage my content." +
-//          "I choose MongoDB, a noSQL database, because it was extremely intuitive and modern, which made it very easy to interact with. " +
-//          "I really like using MongoDB as opposed to SQL, or Postgres, because it was much easier to integrating with my application and interact with " +
-//          "particularly because of Mongoose.",
-//          "User Interface (UI) Design. In University I had taken CIS 2170, a course on user interface design. The skills I learned in this course " +
-//          "were particularly useful in helping me create a UI for this website. It took a lot of playing around with and googling various issues, " +
-//          "such as adapting it to various screen sizes, learning about BootStrap, and more, that helped me create my vision for this webiste."],
-//          url: "https://soorajmodi.com"
-// });
-
-// proj.save(function (err) {
-//   if(err) console.log("Failed MongoDB save");
-// });
-
-// BlogPost.updateOne({ title: 'sooraj modi website' }, { title: 'Sooraj Modi Website' }, function(err, res) {
-//   // Updated at most one doc, `res.modifiedCount` contains the number
-//   // of docs that MongoDB updated
-// });
-
 app.get("/", function(req, res) {
   res.render("home");
 });
@@ -76,24 +36,47 @@ app.get("/experience", function(req, res) {
   res.render("experience");
 });
 
-app.get("/blog", function(req, res) {
-  BlogPost.find({}, function(err, foundBlogPosts) {
-    res.render("blog", {
-      blog: foundBlogPosts
-    });
-  });
-});
+// app.get("/blog", function(req, res) {
+//   BlogPost.find({}, function(err, foundBlogPosts) {
+//     res.render("blog", {
+//       blog: foundBlogPosts
+//     });
+//   });
+// });
 
-app.get("/blog/:post", function(req, res) {
-  BlogPost.findOne({ title:  _.startCase(req.params.post) }, function(err, foundPost) {
-    res.render("post", {
-      title: foundPost.title,
-      date: foundPost.date.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }),
-      body: foundPost.body,
-      url: foundPost.url
-    });
-  });
-});
+// app.get("/blog/compose", function(req, res) {
+//   res.render("compose");
+// });
+
+// app.post("/blog/compose", function(req, res) {
+//   const blogPost = new BlogPost({
+//     title: _.startCase(req.body.composeTitle),
+//     description: req.body.composeDescription,
+//     body: req.body.composeBody,
+//     date: new Date(),
+//     url: req.body.composeURL
+//   });
+//
+//   blogPost.save();
+//
+//   res.redirect("/blog");
+// });
+
+// app.get("/blog/:post", function(req, res) {
+//   BlogPost.findOne({ title: _.startCase(req.params.post) }, function(err, foundPost) {
+//     res.render("post", {
+//       title: foundPost.title,
+//       date: foundPost.date.toLocaleDateString("en-US", {
+//         weekday: "long",
+//         year: "numeric",
+//         month: "long",
+//         day: "numeric"
+//       }),
+//       body: foundPost.body,
+//       url: foundPost.url
+//     });
+//   });
+// });
 
 app.get("/education", function(req, res) {
   res.render("education");
