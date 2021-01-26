36a400496d677899439b3311d54f5032c21214f9
SoorajModi <soomodi@gmail.com>
2021-01-26 00:57:45 -0500

    Add edit and delete post functionality

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const ejs = require("ejs");
+const MarkdownIt = require("markdown-it");
 
+const md = new MarkdownIt();
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,7 +23,7 @@ const blogScheme = {
   title: String,
   description: String,
   date: { type: Date, default: Date.now },
-  body: [String],
+  body: String,
   url: String
 };
 
@@ -36,47 +37,94 @@ app.get("/experience", function(req, res) {
   res.render("experience");
 });
 
-// app.get("/blog", function(req, res) {
-//   BlogPost.find({}, function(err, foundBlogPosts) {
-//     res.render("blog", {
-//       blog: foundBlogPosts
-//     });
-//   });
-// });
+app.get("/blog", function(req, res) {
+  BlogPost.find({}, function(err, foundBlogPost) {
+    res.render("blog", {
+      blog: foundBlogPost
+    });
+  });
+});
 
-// app.get("/blog/compose", function(req, res) {
-//   res.render("compose");
-// });
+app.get("/blog/compose", function(req, res) {
+  res.render("compose", {
+    markdown: MarkdownIt
+  });
+});
 
-// app.post("/blog/compose", function(req, res) {
-//   const blogPost = new BlogPost({
-//     title: _.startCase(req.body.composeTitle),
-//     description: req.body.composeDescription,
-//     body: req.body.composeBody,
-//     date: new Date(),
-//     url: req.body.composeURL
-//   });
-//
-//   blogPost.save();
-//
-//   res.redirect("/blog");
-// });
+app.post("/blog/compose", function(req, res) {
+  const blogPost = new BlogPost({
+    title: _.startCase(req.body.composeTitle),
+    description: req.body.composeDescription,
+    body: req.body.composeBody,
+    date: new Date(),
+    url: req.body.composeURL
+  });
 
-// app.get("/blog/:post", function(req, res) {
-//   BlogPost.findOne({ title: _.startCase(req.params.post) }, function(err, foundPost) {
-//     res.render("post", {
-//       title: foundPost.title,
-//       date: foundPost.date.toLocaleDateString("en-US", {
-//         weekday: "long",
-//         year: "numeric",
-//         month: "long",
-//         day: "numeric"
-//       }),
-//       body: foundPost.body,
-//       url: foundPost.url
-//     });
-//   });
-// });
+  blogPost.save();
+
+  res.redirect("/blog");
+});
+
+app.get("/blog/:post", function(req, res) {
+  BlogPost.findOne({ title: _.startCase(req.params.post) }, function(err, foundPost) {
+    res.render("post", {
+      title: foundPost.title,
+      date: foundPost.date.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      }),
+      body: md.render(foundPost.body),
+      url: foundPost.url
+    });
+  });
+});
+
+app.get("/blog/:post/edit", function(req, res) {
+  BlogPost.findOne({ title: _.startCase(req.params.post) }, function(err, foundPost) {
+    res.render("editPost", {
+      renderURL: req.params.post,
+      title: foundPost.title,
+      date: foundPost.date,
+      description: foundPost.description,
+      body: foundPost.body,
+      url: foundPost.url
+    });
+  });
+});
+
+app.post("/blog/:post/edit", function(req, res) {
+  BlogPost.findOneAndUpdate({ title: _.startCase(req.params.post) }, {
+    title: _.startCase(req.body.editTitle),
+    description: req.body.editDescription,
+    body: req.body.editBody,
+    date: req.body.editDate,
+    url: req.body.editURL
+  }, {}, function() {
+  });
+
+  res.redirect("/blog/" + req.body.editTitle);
+});
+
+app.get("/blog/:post/delete", function(req, res) {
+  BlogPost.findOne({ title: _.startCase(req.params.post) }, function(err, foundPost) {
+    res.render("deletePost", {
+      renderURL: req.params.post,
+      title: foundPost.title,
+      date: foundPost.date,
+      description: foundPost.description,
+      body: foundPost.body,
+      url: foundPost.url
+    });
+  });
+});
+
+app.post("/blog/:post/delete", function(req, res) {
+  BlogPost.findOneAndDelete({ title: _.startCase(req.params.post) }, {}, function() {
+  });
+  res.redirect("/blog");
+});
 
 app.get("/education", function(req, res) {
   res.render("education");
