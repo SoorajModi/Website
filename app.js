83169c93f10c7f1acaee3e1612e6b4d171968bf4
SoorajModi <soomodi@gmail.com>
2021-01-31 20:22:05 -0500

    Add authentication to manage blog

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 const MarkdownIt = require("markdown-it");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const md = new MarkdownIt();
 const app = express();
@@ -13,13 +16,21 @@ app.set("view engine", "ejs");
 app.use(express.static("static"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true
-}).then((r) => console.log(`Successfully connected to MongoDB: ${r}`))
-  .catch((e) => console.log(`Error starting up mongo: ${e}`));
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log(`Successfully connected to MongoDB`))
+  .catch((e) => console.log(`Error connecting to MongoDB: ${e}`));
 
-const blogScheme = {
+const blogSchema = {
   title: String,
   description: String,
   date: { type: Date, default: Date.now },
@@ -27,7 +38,28 @@ const blogScheme = {
   url: String
 };
 
-const BlogPost = mongoose.model("BlogPost", blogScheme);
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose); // Used to hash and salt users, and save to MongoDB
+
+const BlogPost = mongoose.model("BlogPost", blogSchema);
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// User.register({username: process.env.USERNAME}, process.env.PASSWORD, function(err) {
+//   if(err) {
+//     console.log("Error: " + err);
+//   } else {
+//     console.log("Successfully added user");
+//   }
+// });
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -45,24 +77,59 @@ app.get("/blog", function(req, res) {
   });
 });
 
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post("/login", function(req, res) {
+  passport.authenticate("local", {}, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.redirect("/login");
+    }
+
+    if (!user) {
+      console.log("Invalid username");
+      return res.redirect("/login");
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        console.log("Error while logging in: " + err);
+      }
+      return res.redirect("/blog");
+    });
+  })(req, res);
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/blog");
+});
+
 app.get("/blog/compose", function(req, res) {
-  res.render("compose", {
-    markdown: MarkdownIt
-  });
+  if (req.isAuthenticated()) {
+    res.render("compose", {
+      markdown: MarkdownIt
+    });
+  } else {
+    res.redirect("/404");
+  }
 });
 
 app.post("/blog/compose", function(req, res) {
-  const blogPost = new BlogPost({
-    title: _.startCase(req.body.composeTitle),
-    description: req.body.composeDescription,
-    body: req.body.composeBody,
-    date: new Date(),
-    url: (req.body.composeTitle).replace(/\s+/g, '-').toLowerCase()
-  });
+  if (req.isAuthenticated()) {
+    const blogPost = new BlogPost({
+      title: _.startCase(req.body.composeTitle),
+      description: req.body.composeDescription,
+      body: req.body.composeBody,
+      date: new Date(),
+      url: (req.body.composeTitle).replace(/\s+/g, "-").toLowerCase()
+    });
 
-  blogPost.save();
-
-  res.redirect("/blog");
+    blogPost.save((r) => console.log("New blog post added: " + r));
+    res.redirect("/blog");
+  }
 });
 
 app.get("/blog/:post", function(req, res) {
@@ -81,52 +148,68 @@ app.get("/blog/:post", function(req, res) {
 });
 
 app.get("/blog/:post/edit", function(req, res) {
-  BlogPost.findOne({ url: req.params.post }, function(err, foundPost) {
-    if (!foundPost) {
-      res.redirect("/404");
-    } else {
-      res.render("editPost", {
-        url: foundPost.url,
+  if (req.isAuthenticated()) {
+    BlogPost.findOne({ url: req.params.post }, function(err, foundPost) {
+      if (!foundPost) {
+        res.redirect("/404");
+      } else {
+        res.render("editPost", {
+          url: foundPost.url,
+          title: foundPost.title,
+          date: foundPost.date,
+          description: foundPost.description,
+          body: foundPost.body
+        });
+      }
+    });
+  } else {
+    res.redirect("/blog/" + req.params.post);
+  }
+});
+
+app.post("/blog/:post/edit", function(req, res) {
+  if (req.isAuthenticated()) {
+    let newURL = (req.body.editTitle).replace(/\s+/g, "-").toLowerCase();
+
+    BlogPost.findOneAndUpdate({ url: req.params.post }, {
+      title: _.startCase(req.body.editTitle),
+      description: req.body.editDescription,
+      body: req.body.editBody,
+      date: req.body.editDate,
+      url: newURL
+    }, {}, function() {
+    });
+
+    res.redirect("/blog/" + newURL);
+  } else {
+    res.redirect("/blog/" + req.params.post);
+  }
+});
+
+app.get("/blog/:post/delete", function(req, res) {
+  if (req.isAuthenticated()) {
+    BlogPost.findOne({ url: req.params.post }, function(err, foundPost) {
+      res.render("deletePost", {
+        renderURL: req.params.post,
         title: foundPost.title,
         date: foundPost.date,
         description: foundPost.description,
         body: foundPost.body
       });
-    }
-  });
-});
-
-app.post("/blog/:post/edit", function(req, res) {
-  let newURL = (req.body.editTitle).replace(/\s+/g, '-').toLowerCase();
-
-  BlogPost.findOneAndUpdate({ url: req.params.post }, {
-    title: _.startCase(req.body.editTitle),
-    description: req.body.editDescription,
-    body: req.body.editBody,
-    date: req.body.editDate,
-    url: newURL
-  }, {}, function() {
-  });
-
-  res.redirect("/blog/" + newURL);
-});
-
-app.get("/blog/:post/delete", function(req, res) {
-  BlogPost.findOne({ url: req.params.post }, function(err, foundPost) {
-    res.render("deletePost", {
-      renderURL: req.params.post,
-      title: foundPost.title,
-      date: foundPost.date,
-      description: foundPost.description,
-      body: foundPost.body
     });
-  });
+  } else {
+    res.redirect("/blog/" + req.params.post);
+  }
 });
 
 app.post("/blog/:post/delete", function(req, res) {
-  BlogPost.findOneAndDelete({ url: req.params.post }, {}, function() {
-  });
-  res.redirect("/blog");
+  if (req.isAuthenticated()) {
+    BlogPost.findOneAndDelete({ url: req.params.post }, {}, function() {
+    });
+    res.redirect("/blog");
+  } else {
+    res.redirect("/blog/" + req.params.post);
+  }
 });
 
 app.get("/education", function(req, res) {
