31ae6ace44bacfa5dd978a6faedcaf107bd08356
SoorajModi <soomodi@gmail.com>
2021-01-31 23:00:09 -0500

    Update project for release

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("static"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  secret: "Our little secret.",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false
 }));
