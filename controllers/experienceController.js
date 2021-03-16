40a614dbf3abd55bb49e9fe8284ce19cc2bbf605
SoorajModi <soomodi@gmail.com>
2021-03-16 19:49:39 -0400

    Improve CSS for experience page

--- controllers/experienceController.js
+++ controllers/experienceController.js
@@ -1,7 +1,30 @@
 const ExperienceController = {
   get(req, res) {
-    res.render('experience');
-  },
+    res.render("experience", {
+      experiences: [
+        {
+          title: "Title",
+          subheading: "Subheading",
+          content: "Content",
+          uuid: "123456789"
+        },
+        {
+          title: "Title",
+          subheading: "Subheading",
+          content: "Content",
+          uuid: "234567891"
+        }
+      ],
+      skillsLeft: [
+        "skill1",
+        "skill2"
+      ],
+      skillsRight: [
+        "skill3",
+        "skill4"
+      ]
+    });
+  }
 };
 
 module.exports = ExperienceController;
