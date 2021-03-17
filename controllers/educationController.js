7078f7bdf881b581b7b0a0b5ebfda998e31d9791
SoorajModi <soomodi@gmail.com>
2021-03-16 23:53:57 -0400

    Update site to use new UI with dynamic contents

--- controllers/educationController.js
+++ controllers/educationController.js
@@ -1,7 +1,28 @@
 const EducationController = {
   get(req, res) {
-    res.render('education');
-  },
+    res.render("education", {
+      education: [
+        {
+          title: "Title",
+          subheading: "Sub",
+          uuid: "12345",
+          content: "content"
+        },
+        {
+          title: "Title2",
+          subheading: "Sub2",
+          uuid: "23451",
+          content: "content2"
+        }
+      ],
+      certifications: [
+        {
+          name: "Cert",
+          link: "https://google.com"
+        }
+      ]
+    });
+  }
 };
 
 module.exports = EducationController;
--- controllers/volunteerController.js
+++ controllers/volunteerController.js
@@ -1,7 +1,22 @@
 const VolunteerController = {
   get(req, res) {
-    res.render('volunteering');
-  },
+    res.render("volunteering", {
+      volunteering: [
+        {
+          title: "Title",
+          subheading: "Sub",
+          uuid: "12345",
+          content: "content"
+        },
+        {
+          title: "Title2",
+          subheading: "Sub2",
+          uuid: "23451",
+          content: "content2"
+        }
+      ]
+    });
+  }
 };
 
 module.exports = VolunteerController;
